d9f434533d38542a58fe840d2bd97bea18a1b717
Josh Campbell <josh.lee.campbell@gmail.com>
2026-05-30 23:24:33 -0500

    chore: prepare 0.1.1 release metadata

--- include/firmware_version.h
+++ include/firmware_version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-static constexpr char kSnowLeopardFirmwareVersion[] = "0.1.0-dev";
+static constexpr char kSnowLeopardFirmwareVersion[] = "0.1.1";
 static constexpr char kSnowLeopardReleaseManifestUrl[] =
   "https://thingengineer.github.io/SnowLeopard/release-data/current.json";
