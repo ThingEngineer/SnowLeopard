612b208d09308f47eb2d2d994df181bffaf4a56d
Josh Campbell <josh.lee.campbell@gmail.com>
2026-06-01 11:29:02 -0500

    feat(release): update firmware version to 0.1.2 with release notes for improved history page and update behavior

--- include/firmware_version.h
+++ include/firmware_version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-static constexpr char kSnowLeopardFirmwareVersion[] = "0.1.1";
+static constexpr char kSnowLeopardFirmwareVersion[] = "0.1.2";
 static constexpr char kSnowLeopardReleaseManifestUrl[] =
   "https://thingengineer.github.io/SnowLeopard/release-data/current.json";
